60fa2a8ed8c54c776fd79c566513ffe16af17d03
Nikko Aldrich Monroe <monroenikkochase@gmail.com>
2022-05-21 16:28:20 +0800

    Initialize 1.x

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="container">
+    <div>
+      <strong>Home Page</strong>
+      <pre>Both guests and logged in users can access!</pre>
+      <nuxt-link to="/login">Login</nuxt-link>
+    </div>
+  </section>
+</template>
